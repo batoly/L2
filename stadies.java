5b9b04898734e10f48e1b788696e9c2f00799370
Batool sharafaldeen <101997272+batoly@users.noreply.github.com>
2022-03-22 20:52:20 +0300

    Add files via upload

---
+++ stadies.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+/**
+ * Created by Lenovo on 1/29/2022.
+ */
+public class stadies {
+    public static void main(String[] args) {
+        int a[]={11,12,13,14,15};
+        System.out.println(Arrays.toString(a));
+        System.out.println("Araay After Right");
+
+    }
+}
